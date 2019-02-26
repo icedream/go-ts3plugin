a0e3787a6b1e7849a99123fc64eaec195384b7cc
Carl Kittelberger <icedream@icedream.pw>
2019-02-26 04:34:56 +0100

    Change download URL for plugin SDK.

--- gen_pluginsdk.go
+++ gen_pluginsdk.go
@@ -1,3 +1,3 @@
 package ts3plugin
 
-//go:generate go run ./internal/generate/download_pluginsdk/download_pluginsdk.go http://dl.4players.de/ts/client/pluginsdk/pluginsdk_3.1.1.1.zip ./
+//go:generate go run ./internal/generate/download_pluginsdk/download_pluginsdk.go https://files.teamspeak-services.com/releases/sdk/plugin_sdk/pluginsdk_3.1.1.1.zip ./
