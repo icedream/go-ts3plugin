f4bf3b39a0e9f76fea7019feb48e4150e813a512
Carl Kittelberger <icedream@icedream.pw>
2026-06-09 04:00:46 +0200

    Update download URLs for plugin SDK.

--- gen_pluginsdk.go
+++ gen_pluginsdk.go
@@ -1,7 +1,16 @@
 package ts3plugin
 
+// for v26:
+//go:generate go run ./internal/generate/download_pluginsdk/download_pluginsdk.go https://github.com/teamspeak/ts3client-pluginsdk/archive/refs/tags/26.zip ./pluginsdk
+
 // for v24:
-//go:generate go run ./internal/generate/download_pluginsdk/download_pluginsdk.go https://github.com/TeamSpeak-Systems/ts3client-pluginsdk/archive/a39d50383b4a023941c31c08fe0c9766b149ed01.zip ./pluginsdk
+// go run ./internal/generate/download_pluginsdk/download_pluginsdk.go https://github.com/teamspeak/ts3client-pluginsdk/archive/refs/tags/24.zip ./pluginsdk
 
 // for v23:
 // go run ./internal/generate/download_pluginsdk/download_pluginsdk.go https://github.com/TeamSpeak-Systems/ts3client-pluginsdk/releases/download/23/pluginsdk_api_23.zip ./pluginsdk
+
+// for v22:
+// go run ./internal/generate/download_pluginsdk/download_pluginsdk.go https://github.com/TeamSpeak-Systems/ts3client-pluginsdk/archive/ae1e04b87fcdf256b03127c2716e099ea09f2010.zip ./pluginsdk
+
+// for v21:
+// go run ./internal/generate/download_pluginsdk/download_pluginsdk.go https://github.com/TeamSpeak-Systems/ts3client-pluginsdk/archive/db1fc56f55de51f258e30b04e3637b5e8d32af9a.zip ./pluginsdk
